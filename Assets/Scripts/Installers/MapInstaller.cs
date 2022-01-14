4c30b7e2dd15e24583add46d651032a751eb532b
Yana Dymura <talioo1997@gmail.com>
2022-01-15 00:03:45 +0200

    Improve map

---
+++ Assets/Scripts/Installers/MapInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Installers
+{
+    public class MapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
